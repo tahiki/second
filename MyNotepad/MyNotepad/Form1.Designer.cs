497e22e735643f230d99b10ccf57c6420af89231
tahiki <tahikiart@gmail.com>
2024-09-14 01:02:52 +0500

    зеленая и нет кнопки закрыть

--- MyNotepad/MyNotepad/Form1.Designer.cs
+++ MyNotepad/MyNotepad/Form1.Designer.cs
@@ -28,26 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(232, 79);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 62);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Закрыть";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(643, 225);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -56,8 +44,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
